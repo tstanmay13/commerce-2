136d20799a58f2ac053500e6c619b20f03cc136b
tstanmay13 <tstanmay13@gmail.com>
2022-07-14 21:30:47 -0500

    shell done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import './index.css';
 import Nav from './componenets/Nav';
 import Landing from './componenets/Landing';
 import Highlights from './componenets/Highlights';
+import Featured from './componenets/Featured';
+import Discounted from './componenets/Discounted';
+import Explore from './componenets/Explore';
+import Footer from './componenets/Footer';
 
 function App() {
   return (
@@ -10,6 +14,10 @@ function App() {
       <Nav/>
       <Landing/>
       <Highlights/>
+      <Featured />
+      <Discounted />
+      <Explore />
+      <Footer />
     </div>
   );
 }
